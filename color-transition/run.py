b1d7b989ab3aa54f3fdeb792f95b0b90586b7330
mgsosna <matt.g.sosna@gmail.com>
2024-01-01 19:03:14 -0500

    Tidy up

--- color-transition/run.py
+++ color-transition/run.py
@@ -9,6 +9,7 @@ from helpers import gini, main_add_circles
 a_color = "#0c8599"
 b_color = "#f08c00"
 
+# Generate data
 x = np.linspace(0, 1, 8000)
 y = gini(x)
 
@@ -16,26 +17,33 @@ y = gini(x)
 points = np.array([x, 2*y]).T.reshape(-1, 1, 2)
 segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
-cmap = LinearSegmentedColormap.from_list('custom blue', [a_color, b_color], N=1000)
-
 # Generate plot
 plt.figure(figsize=(5,5))
-lc = LineCollection(segments, cmap=cmap,
-    norm=plt.Normalize(0, 1))
-lc.set_array(x)
-lc.set_linewidth(3)
-
 plt.xlim(-0.03, 1.03)
 plt.ylim(-0.03, 1.03)
-plt.gca().add_collection(lc)
-
 plt.xlabel("$p_▲$", fontsize=16)
 plt.ylabel("Gini impurity", fontsize=16)
 
-# Overwrite tickmarks
-plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0], labels=[0, 0.1, 0.2, 0.3, 0.4, 0.5])
+# Overwrite tickmarks so they're accurate
+plt.yticks(
+    [0, 0.2, 0.4, 0.6, 0.8, 1.0],
+    labels=[0, 0.1, 0.2, 0.3, 0.4, 0.5]
+)
+
+# Generate line
+cmap = LinearSegmentedColormap.from_list(
+    'custom blue',
+    [a_color, b_color],
+    N=1000
+)
+lc = LineCollection(segments, cmap=cmap,
+    norm=plt.Normalize(0, 1))
+lc.set_array(x)
+lc.set_linewidth(4)
+plt.gca().add_collection(lc)
 
 # Add circles
 main_add_circles(a_color, b_color)
 
+# Save figure
 plt.savefig("gini_impurity.png", dpi=400, bbox_inches='tight')
